db9d7db0a233231a0abfd2a89004c58152f32f10
Christian <ck6202@rit.edu>
2019-09-04 19:27:11 -0400

    Set up Eclipse workspace

---
+++ measure.cpp
@@ -0,0 +1,16 @@
+/*
+ * measure.cpp
+ *
+ *  Created on: Sep 4, 2019
+ *      Author: Christian Kobb & Liam Callahan
+ */
+#ifndef MEASURE_CPP_
+#define MEASURE_CPP_
+
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "hello world" << endl;
+	return 0;
+}
+#endif /* MEASURE_CPP_ */
